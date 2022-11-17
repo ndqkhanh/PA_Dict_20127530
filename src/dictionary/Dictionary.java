ed15393c514fe96446268f174fa28a5a7993da74
Nguyễn Đinh Quang Khánh <20127530@student.hcmus.edu.vn>
2022-11-17 22:53:10 +0700

    build: build add,edit,delete slang word

--- src/dictionary/Dictionary.java
+++ src/dictionary/Dictionary.java
@@ -14,7 +14,7 @@ public class Dictionary {
      * Hash map from slang words to their definitions
      */
     private HashMap<String, HashSet<String>> dictionary;
-    
+
     /**
      * Default constructor
      */
@@ -23,13 +23,87 @@ public class Dictionary {
     }
 
     /**
-     * @return a dictionary list with slang words and their definitions
+     * @return A dictionary list with slang words and their definitions
      */
     public HashMap<String, HashSet<String>> getDictionary() {
         return dictionary;
     }
 
+    /**
+     * @param dictionary Hash map value we want to set to the dictionary
+     */
     public void setDictionary(HashMap<String, HashSet<String>> dictionary) {
         this.dictionary = dictionary;
+    }
+
+//    /**
+//     * @param slang Slang word we want to search from our dictionary
+//     * @return ArrayList of slang words and definitions that contain the slang parameter pass in
+//     */
+//    public ArrayList<Map.Entry<String, HashSet<String>>> searchDefsBySlang(String slang) {
+//        return dictionary
+//                .entrySet()
+//                .stream()
+//                .filter(record -> record.getKey().contains(slang))
+//                .collect(Collectors.toCollection(ArrayList::new));
+//    }
+//
+//
+//    /**
+//     * @param def Definition we want to search from our dictionary
+//     * @return ArrayList of slang words and definitions that contain the def parameter pass in
+//     */
+//    public ArrayList<Map.Entry<String, HashSet<String>>> searchSlangsByDef(String def) {
+//        String[] arrOfStr = def.split(" ");
+//        return dictionary
+//                .entrySet()
+//                .stream()
+//                .filter(record -> {
+//                    for (String word : arrOfStr)
+//                        if (record.getValue().contains(word)) return true;
+//                    return false;
+//                }).collect(Collectors.toCollection(ArrayList::new));
+//    }
+
+    /**
+     * @param slang       Slang word we want to add to dictionary
+     * @param def         Definition of above slang word
+     * @param isOverwrite If slang word exist then if isOverwrite == true then
+     *                    the slang word's definition will be overwritten,
+     *                    otherwise the definition will be duplicated
+     */
+    public void addSlangWord(String slang, String def, boolean isOverwrite) {
+        HashSet<String> defsOfSlang = dictionary.get(slang);
+        if (defsOfSlang != null) {
+            if (isOverwrite) {
+                defsOfSlang = new HashSet<>();
+                defsOfSlang.add(def);
+                dictionary.put(slang, defsOfSlang);
+            } else {
+                defsOfSlang.add(def);
+            }
+        } else {
+            defsOfSlang = new HashSet<>();
+            defsOfSlang.add(def);
+            dictionary.put(slang, defsOfSlang);
+        }
+    }
+
+    /**
+     * @param slang  Slang word we want to edit
+     * @param oldDef Old definition of slang word
+     * @param newDef New definition of slang word we want to update
+     */
+    public void editSlangWord(String slang, String oldDef, String newDef) {
+        HashSet<String> defsOfSlang = dictionary.get(slang);
+        boolean result = defsOfSlang.remove(oldDef);
+        if (result) defsOfSlang.add(newDef);
+    }
+
+    /**
+     * @param slang Slang word we want to delete from our dictionary
+     */
+    public void deleteSlangWord(String slang) {
+        dictionary.remove(slang);
     }
 }
---
+++ src/nhap.java
@@ -0,0 +1,49 @@
+/**
+ * PACKAGE_NAME
+ * Created by khanh
+ * Date 11/17/2022 - 10:20 PM
+ * Description: ...
+ */
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Nhap {
+    private static HashMap<String, HashSet<String>> dictionary = new HashMap<>();
+
+    public static void main(String[] args) {
+        Nhap nhap = new Nhap();
+        HashSet<String> test = new HashSet<>();
+        test.add("1");
+        test.add("2");
+        test.add("3");
+        dictionary.put("item1", test);
+        // System.out.println(dictionary.get("item1"));
+        test = new HashSet<>();
+        test.add("4");
+        test.add("5");
+        test.add("6");
+        dictionary.put("item2", test);
+//        nhap.searchSlangsByDef("1 2");
+//         System.out.println(dictionary.get("item1"));
+        System.out.println(nhap.searchSlangsByDef("1 2"));
+    }
+
+    public ArrayList<Map.Entry<String, HashSet<String>>> searchSlangsByDef(String def) {
+        String[] arrOfStr = def.split(" ");
+        return dictionary
+                .entrySet()
+                .stream()
+                .filter(d -> {
+                    for (String word : arrOfStr)
+                        if (d.getValue().contains(word))
+                            return true;
+                    return false;
+                }).collect(Collectors.toCollection(ArrayList::new));
+    }
+}
